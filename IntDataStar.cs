2abd5a3289a46415626c6463e42c20c6b2fdb601
Craig Granger <10992413@uvu.edu>
2024-01-24 22:39:57 -0700

    Float, Int, Trigger Scriptable Objects RULE :)
    Float, Int, Scriptable Objects RULE :)

---
+++ IntDataStar.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+    [CreateAssetMenu(menuName = "IntDataStar")]
+public class StarPowerUp : ScriptableObject
+{
+    public string objectName;
+    public int powerLevel;
+    public bool collideable;
+    
+}
